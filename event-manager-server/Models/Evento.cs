4b6cb0bf40c663458132acead82e831f4ca668f4
apomic80 <michele.aponte@blexin.com>
2020-03-03 09:01:12 +0100

    gestione form

---
+++ event-manager-server/Models/Evento.cs
@@ -0,0 +1,20 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace event_manager_server.Models
+{
+    public class Evento
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage="Il nome è obbligatorio")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage="La località è obbligatoria")]
+        [StringLength(50, ErrorMessage="La lunghezza può essere al massimo di {1} caratteri")]
+        public string Localita { get; set; }
+        public DateTime Data { get; set; }
+        public string Descrizione { get; set; }
+        public string Note { get; set; }
+    }
+}
