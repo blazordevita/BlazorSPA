a5644950f825ecbbd65f2b234fcaace5f4d78ee1
Salvatore Sorrentino <salvatore.sorrentino@live.com>
2022-01-13 11:39:43 +0100

    interop javascript C#

--- event-manager-data/Migrations/20220112154122_DatiEvento2.Designer.cs
+++ event-manager-data/Migrations/20220112154122_DatiEvento2.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using event_manager_data;
 
@@ -10,9 +11,10 @@ using event_manager_data;
 namespace event_manager_data.Migrations
 {
     [DbContext(typeof(EventManagerDbContext))]
-    partial class EventManagerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220112154122_DatiEvento2")]
+    partial class DatiEvento2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.1");
